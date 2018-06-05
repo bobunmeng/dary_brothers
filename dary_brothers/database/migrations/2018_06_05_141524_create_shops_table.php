10caa990de81dcc5d175dc3875d80f0d98d6f528
bobunmeng <bunmeng@mango-byte.com>
2018-06-05 21:44:33 +0700

    add db file

---
+++ dary_brothers/database/migrations/2018_06_05_141524_create_shops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->primary('id');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('en_address');
+            $table->string('kh_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
