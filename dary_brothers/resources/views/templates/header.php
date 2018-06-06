279f4830166f7ebc2d5fe729a7e7796d2beb9ead
bobunmeng <bunmeng@mango-byte.com>
2018-06-06 22:21:03 +0700

    add homepage

---
+++ dary_brothers/resources/views/templates/header.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+  <head>
+    <link href="/css/app.css" rel="stylesheet">
+
+    <style>
+
+      #container {
+        align-items: center;
+      }
+
+      #nav_container {
+        align-items: flex-end;
+        padding: 10px;
+      }
+
+    </style>
+
+  </head>
+  <body>
+      <div class="d-flex justify-content-around" id="container">
+        <div class="d-inline-block">
+          <img src="/storage/sample_mcdonald.jpg" alt="Shop Logo" width="150" height="150" />
+        </div>
+        <div class="d-inline-block" id="nav_container">
+          <div>
+            Create Acount / Login
+          </div>
+          <div>
+            <ul class="nav nav-pills nav-fill">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">PRODUCT</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">OUR SHOP</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
