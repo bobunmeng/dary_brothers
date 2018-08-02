a499ec5693c9711d7e6cf89de423d1e08c9c26d8
Touch <s.chantouch@mango-byte.com>
2018-08-02 07:20:54 +0700

    Changed template

--- shop/resources/views/frontend/layouts/app.blade.php
+++ shop/resources/views/frontend/layouts/app.blade.php
@@ -5,116 +5,101 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Wish shop project">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.png') }}"/>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    {{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
-
-    <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap4/bootstrap.min.css') }}">
-    <link href="{{ asset('plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('plugins/OwlCarousel2-2.2.1/animate.css') }}">
-    <link href="{{ asset('plugins/colorbox/colorbox.css') }}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/main_styles.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/responsive.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/fonts/themify/themify-icons.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/fonts/elegant-font/html-css/style.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/animate/animate.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/css-hamburgers/hamburgers.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/animsition/css/animsition.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/select2/select2.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/daterangepicker/daterangepicker.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/slick/slick.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/lightbox2/css/lightbox.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend/main.css') }}">
+    <!--===============================================================================================-->
 </head>
 <body>
-<div id="app" class="super_container">
+<div id="app" class="animsition">
     @include('frontend.layouts.header')
-    @include('frontend.layouts.menu')
     <main>
-        <!-- Home -->
-
-        <div class="home">
-
-            <!-- Home Slider -->
-
-            <div class="home_slider_container">
-                <div class="owl-carousel owl-theme home_slider">
-
-                    <!-- Home Slider Item -->
-                    <div class="owl-item">
-                        <div class="home_slider_background" style="background-image:url('images/home_slider_1.jpg')"></div>
-                        <div class="home_slider_content">
-                            <div class="home_slider_content_inner">
-                                <div class="home_slider_subtitle">Promo Prices</div>
-                                <div class="home_slider_title">New Collection</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Home Slider Item -->
-                    <div class="owl-item">
-                        <div class="home_slider_background" style="background-image:url(images/home_slider_1.jpg)"></div>
-                        <div class="home_slider_content">
-                            <div class="home_slider_content_inner">
-                                <div class="home_slider_subtitle">Promo Prices</div>
-                                <div class="home_slider_title">New Collection</div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Home Slider Item -->
-                    <div class="owl-item">
-                        <div class="home_slider_background" style="background-image:url(images/home_slider_1.jpg)"></div>
-                        <div class="home_slider_content">
-                            <div class="home_slider_content_inner">
-                                <div class="home_slider_subtitle">Promo Prices</div>
-                                <div class="home_slider_title">New Collection</div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <!-- Home Slider Nav -->
-
-                <div class="home_slider_next d-flex flex-column align-items-center justify-content-center"><img src="images/arrow_r.png" alt=""></div>
-
-                <!-- Home Slider Dots -->
-
-                <div class="home_slider_dots_container">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <div class="home_slider_dots">
-                                    <ul id="home_slider_custom_dots" class="home_slider_custom_dots">
-                                        <li class="home_slider_custom_dot active">01.<div></div></li>
-                                        <li class="home_slider_custom_dot">02.<div></div></li>
-                                        <li class="home_slider_custom_dot">03.<div></div></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         @yield('content')
 
-        @include('frontend.layouts.newsletter')
         @include('frontend.layouts.footer')
+
+        @include('frontend.layouts.backtotop')
+
+        <div id="dropDownSelect1"></div>
     </main>
 </div>
 </body>
-<script src="{{ asset('js/frontend/jquery-3.2.1.min.js') }}"></script>
-<script src="{{ asset('css/bootstrap4/popper.js') }}"></script>
-<script src="{{ asset('css/bootstrap4/bootstrap.min.js') }}"></script>
-<script src="{{ asset('plugins/OwlCarousel2-2.2.1/owl.carousel.js') }}"></script>
-<script src="{{ asset('plugins/easing/easing.js') }}"></script>
-<script src="{{ asset('plugins/parallax-js-master/parallax.min.js') }}"></script>
-<script src="{{ asset('plugins/colorbox/jquery.colorbox-min.js') }}"></script>
-<script src="{{ asset('js/frontend/custom.js') }}"></script>
+
+<!--===============================================================================================-->
+<script type="text/javascript" src="{{ asset('vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="{{ asset('vendor/animsition/js/animsition.min.js') }}"></script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="{{ asset('vendor/bootstrap/js/popper.js') }}"></script>
+<script type="text/javascript" src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="{{ asset('vendor/select2/select2.min.js') }}"></script>
+<script type="text/javascript">
+  $('.selection-1').select2({
+    minimumResultsForSearch: 20,
+    dropdownParent: $('#dropDownSelect1')
+  });
+</script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="{{ asset('vendor/slick/slick.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/frontend/slick-custom.js') }}"></script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="{{ asset('vendor/countdowntime/countdowntime.js') }}"></script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="{{ asset('vendor/lightbox2/js/lightbox.min.js') }}"></script>
+<!--===============================================================================================-->
+<script type="text/javascript" src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+<script type="text/javascript">
+  $('.block2-btn-addcart').each(function () {
+    var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+    $(this).on('click', function () {
+      swal(nameProduct, 'is added to cart !', 'success');
+    });
+  });
+
+  $('.block2-btn-addwishlist').each(function () {
+    var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
+    $(this).on('click', function () {
+      swal(nameProduct, 'is added to wishlist !', 'success');
+    });
+  });
+</script>
+
+<!--===============================================================================================-->
+<script src="{{ asset('js/frontend/main.js') }}"></script>
+
 </html>
