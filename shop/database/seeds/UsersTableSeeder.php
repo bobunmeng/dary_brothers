37b67451715fe8c3eaacf1fad137203b39692cf0
Touch <s.chantouch@mango-byte.com>
2018-08-04 14:02:19 +0700

    Add user permission and role

---
+++ shop/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'super.admin@example.com',
+            'password' => bcrypt('secret')
+        ]);
+    }
+}
